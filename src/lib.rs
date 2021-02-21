3d44b324864161b71d2af0b47eeb23d468c08ea5
Odin Dutton <odindutton@gmail.com>
2021-02-22 09:38:46 +1100

    WIP
    [ci skip]

--- src/lib.rs
+++ src/lib.rs
@@ -7,94 +7,79 @@
 //!
 //! [blog_post]: https://www.yegor256.com/2014/11/14/hits-of-code.html
 //! [ruby_version]: https://github.com/yegor256/hoc
-use std::{process, process::Command, str};
+use git2::{DiffOptions, Repository};
 
-const COMMAND_ARGUMENTS: &'static [&'static str] = &[
-    "log",
-    "--pretty=tformat:",
-    "--numstat",
-    "--ignore-space-change",
-    "--ignore-all-space",
-    "--ignore-submodules",
-    "--no-color",
-    "--diff-filter=ACDM",
-];
+pub fn hoc(_find_renames_and_copies: bool) -> Result<u64, git2::Error> {
+    let repo = Repository::open(".")?;
 
-pub fn run(find_renames_and_copies: bool) {
-    let mut extra_command_arguments: &'static [&'static str] = &[];
-    // Enabling this option causes the git command to be significantly slower.
-    if find_renames_and_copies {
-        // From the git-log man page:
-        //
-        // -M[<n>], --find-renames[=<n>]
-        //     If generating diffs, detect and report renames for each commit. For following files
-        //     across renames while traversing history, see --follow. If n is specified, it is a
-        //     threshold on the similarity index (i.e. amount of addition/deletions compared to the
-        //     file's size). For example, -M90% means Git should consider a delete/add pair to be a
-        //     rename if more than 90% of the file hasn't changed. Without a % sign, the number is
-        //     to be read as a fraction, with a decimal point before it. I.e., -M5 becomes 0.5, and
-        //     is thus the same as -M50%. Similarly, -M05 is the same as -M5%. To limit detection
-        //     to exact renames, use -M100%. The default similarity index is 50%.
-        //
-        // -C[<n>], --find-copies[=<n>]
-        //     Detect copies as well as renames. See also --find-copies-harder. If n is specified,
-        //     it has the same meaning as for -M<n>.
-        //
-        // --find-copies-harder
-        //     For performance reasons, by default, -C option finds copies only if the original
-        //     file of the copy was modified in the same changeset. This flag makes the command
-        //     inspect unmodified files as candidates for the source of copy. This is a very
-        //     expensive operation for large projects, so use it with caution. Giving more than one
-        //     -C option has the same effect.
-        extra_command_arguments = &["--find-renames", "--find-copies-harder"];
-    }
+    // "git log",
+    // "--pretty=tformat:",
+    // "--numstat",
+    // "--ignore-space-change",
+    // "--ignore-all-space",
+    // "--ignore-submodules",
+    // "--no-color",
+    // "--diff-filter=ACDM", // added copied deleted modified
 
-    let output = Command::new("git")
-        .args([COMMAND_ARGUMENTS, extra_command_arguments].concat())
-        .output()
-        .expect("git command failed");
+    // TODO: Are there other things we should ignore? I.e. ignore_filemode()
+    let mut diffopts = DiffOptions::new();
+    diffopts.ignore_whitespace(true); // ASSUMING: --ignore-all-space (might also need ignore_whitespace_eol?)
+    diffopts.ignore_whitespace_change(true); // ASSUMING: --ignore-space-change
+    diffopts.ignore_submodules(true); // ASSUMING: --ignore-submodules
 
-    // Output format (whitespace formatting added for readability):
+    // TODO: find_renames_and_copies
     //
-    // 21   \t  0   \t  .gitignore  \n
-    // 7    \t  11  \t  Cargo.toml  \n
-    // 3    \t  0   \t  src/main.rs \n
-    if output.status.success() {
-        let mut total: usize = 0;
-        let mut num: Vec<u8> = vec![];
-        let mut in_word = false;
+    // Enabling this option causes the git command to be significantly slower.
+    //
+    // From the git-log man page:
+    //
+    // -M[<n>], --find-renames[=<n>]
+    //     If generating diffs, detect and report renames for each commit. For following files
+    //     across renames while traversing history, see --follow. If n is specified, it is a
+    //     threshold on the similarity index (i.e. amount of addition/deletions compared to the
+    //     file's size). For example, -M90% means Git should consider a delete/add pair to be a
+    //     rename if more than 90% of the file hasn't changed. Without a % sign, the number is
+    //     to be read as a fraction, with a decimal point before it. I.e., -M5 becomes 0.5, and
+    //     is thus the same as -M50%. Similarly, -M05 is the same as -M5%. To limit detection
+    //     to exact renames, use -M100%. The default similarity index is 50%.
+    //
+    // -C[<n>], --find-copies[=<n>]
+    //     Detect copies as well as renames. See also --find-copies-harder. If n is specified,
+    //     it has the same meaning as for -M<n>.
+    //
+    // --find-copies-harder
+    //     For performance reasons, by default, -C option finds copies only if the original
+    //     file of the copy was modified in the same changeset. This flag makes the command
+    //     inspect unmodified files as candidates for the source of copy. This is a very
+    //     expensive operation for large projects, so use it with caution. Giving more than one
+    //     -C option has the same effect.
 
-        for c in output.stdout.iter() {
-            match c {
-                b'\t' => {
-                    if !num.is_empty() {
-                        total += integer_from_slice(num.as_slice()) as usize;
-                        num.clear();
-                    }
-                }
-                b'\n' => in_word = false,
-                b'0'..=b'9' => {
-                    if !in_word {
-                        num.push(*c);
-                    }
-                }
-                _ => in_word = true,
-            }
-        }
-        println!("{}", total);
-    } else {
-        if let Some(code) = output.status.code() {
-            eprintln!("git command exited with status {}", code)
+    let mut c: u64 = 0;
+
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_head()?;
+
+    let commits = revwalk.filter_map(|id| id.map(|id| repo.find_commit(id)).ok());
+
+    for commit in commits {
+        let commit = commit?;
+
+        let old_tree = if commit.parents().len() == 1 {
+            let parent = commit.parent(0)?;
+            Some(parent.tree()?)
         } else {
-            eprintln!("git command killed with a signal")
-        }
-        process::exit(1);
-    }
-}
+            None
+        };
+        let new_tree = commit.tree()?;
 
-fn integer_from_slice(slice: &[u8]) -> usize {
-    str::from_utf8(slice)
-        .expect("not a string")
-        .parse::<usize>()
-        .expect("not a number")
+        let stats = repo
+            .diff_tree_to_tree(old_tree.as_ref(), Some(&new_tree), Some(&mut diffopts))?
+            .stats()?;
+
+        c += stats.files_changed() as u64;
+        c += stats.insertions() as u64;
+        c += stats.deletions() as u64;
+    }
+
+    Ok(c)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::{env, path::Path, process};
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() == 1 {
-        hoc::run(false);
+    let find_renames_and_copies = if args.len() == 1 {
+        false
     } else if args[1] == "--find-renames-and-copies" {
-        hoc::run(true);
+        true
     } else {
         let mut name = None;
         if let Some(file_name) = Path::new(&args[0]).file_name() {
@@ -18,5 +18,13 @@ fn main() {
             name.unwrap_or("hoc")
         );
         process::exit(1);
+    };
+
+    match hoc::hoc(find_renames_and_copies) {
+        Ok(count) => println!("{}", count),
+        Err(_) => {
+            eprintln!("an error occurred");
+            process::exit(1);
+        }
     }
 }
